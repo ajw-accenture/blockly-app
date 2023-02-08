24ed6918cb3fe7579bdf66bd22bbd3c6e4a3913c
ajw-accenture <ajw-accenture@users.noreply.github.com>
2023-02-08 12:02:46 -0600

    misc: webpack changes dist folder by BUILD_TARGET

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
+const buildTarget = process.env['BUILD_TARGET'] ?? 'DEV'
+const distDirectory = buildTarget === 'DEV' ? 'dist' : 'build';
+
 module.exports = {
   mode: "production",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, distDirectory),
   },
   plugins: [
     new HtmlWebpackPlugin({
